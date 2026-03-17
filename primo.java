fe453ef432fe46648d60ea65561e63e7c4e1a965
git-key <pruebagpt01.1@gmail.com>
2026-03-17 19:14:29 +0100

    nueva versión programa primo, edt, lista

--- primo.java
+++ primo.java
@@ -1,1 +1,26 @@
-kjs
+import java.util.Scanner;
+
+public class Primo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero: ");
+        int num = sc.nextInt();
+
+        boolean esPrimo = esPrimo(num);
+
+        System.out.println(esPrimo ? "El numero es primo" : "El numero no es primo");
+
+        sc.close();
+    }
+
+    public static boolean esPrimo(int num) {
+        if (num <= 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+
+        return true;
+    }
+}
