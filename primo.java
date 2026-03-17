212ae7e62025ae18484f96bba433f7ed670de1e5
Jhonatan Barrios <jduenasb@fcpn.edu.bo>
2026-03-16 21:09:16 -0400

    Desarrollo programa primo, commandos y lista

--- primo.java
+++ primo.java
@@ -1,1 +1,28 @@
-kjs
+import java.util.Scanner;
+
+public class Primo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        boolean esPrimo = true;
+
+        System.out.print("Ingrese un numero: ");
+        num = sc.nextInt();
+
+        if (num <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+
+        if (esPrimo)
+            System.out.println("El numero es primo");
+        else
+            System.out.println("El numero no es primo");
+    }
+}
